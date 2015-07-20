c8300b0fb0f1a4cd98f764e6cef43580a6c23e34
iom <iom.mailbox@gmail.com>
2015-07-20 20:54:51 +0300

    Inital commit. Hardcoded 50% volume level setting.

---
+++ VolumeSetter/Program.cs
@@ -0,0 +1,28 @@
+﻿using NAudio.CoreAudioApi;
+
+namespace VolumeSetter
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int level50 = 50;
+            SetVolumeLevel(level50);
+        }
+
+        private static void SetVolumeLevel(int value)
+        {
+            try
+            {
+                var devEnum = new MMDeviceEnumerator();
+                MMDevice device = devEnum.GetDefaultAudioEndpoint(0, (Role) 1);
+
+                device.AudioEndpointVolume.MasterVolumeLevelScalar = value/100.0f;
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+    }
+}
