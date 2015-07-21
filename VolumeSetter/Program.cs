84787be45ff8e4e65871bd710e70bac5f7681324
iom <iom.mailbox@gmail.com>
2015-07-21 12:40:38 +0300

    Added command line argument - desired volume level.
    Switched to AnyCPU platform (from x64).

--- VolumeSetter/Program.cs
+++ VolumeSetter/Program.cs
@@ -1,13 +1,27 @@
-﻿using NAudio.CoreAudioApi;
+﻿using System;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
 
 namespace VolumeSetter
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            const int level50 = 50;
-            SetVolumeLevel(level50);
+            if (args.Length < 1)
+            {
+                MessageBox.Show("Usage: VolumeSetter <num>", "Usage", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Environment.Exit(1);
+            }
+            int level;
+            bool result = int.TryParse(args[0], out level);
+            if (!result || level < 0 || level > 100)
+            {
+                MessageBox.Show("Volume level must be from 0 to 100", "Parameter error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                Environment.Exit(1);
+            }
+            SetVolumeLevel(level);
         }
 
         private static void SetVolumeLevel(int value)
@@ -16,12 +30,12 @@ namespace VolumeSetter
             {
                 var devEnum = new MMDeviceEnumerator();
                 MMDevice device = devEnum.GetDefaultAudioEndpoint(0, (Role) 1);
-
                 device.AudioEndpointVolume.MasterVolumeLevelScalar = value/100.0f;
             }
-            catch
+            catch(Exception e)
             {
-                // ignored
+                MessageBox.Show("Error: " + e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(1);
             }
         }
     }
